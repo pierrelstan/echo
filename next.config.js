dc9a252c3ad69d5a43ca580c8122235a0dab5b70
Stanley Pierre Louis <stanleypl75@gmail.com>
2022-09-05 13:15:06 -0400

    feat: make nextjs accept svg file

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    // formats: ['image/svg', 'image/png'],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
