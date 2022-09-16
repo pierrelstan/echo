22b42e8ed01c803abf8a4e018b7b86f76dda7e20
Stanley Pierre Louis <stanleypl75@gmail.com>
2022-09-16 13:45:42 -0400

    fix: update getproduct

--- utils/getProduct.util.ts
+++ utils/getProduct.util.ts
@@ -11,7 +11,7 @@ async function getProducts() {
     if (error) {
       const {
         data: { products },
-      } = await axios.get(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/product`);
+      } = await axios.get(`/api/product`);
 
       return products;
     }
