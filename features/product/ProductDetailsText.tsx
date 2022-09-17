91b348f5195f71019ca55cc0c10af2d97eb1a77f
Stanley Pierre Louis <stanleypl75@gmail.com>
2022-09-17 17:07:41 -0400

    feat: add get product by id

---
+++ features/product/ProductDetailsText.tsx
@@ -0,0 +1,38 @@
+import Divider from "@mui/material/Divider";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import { Product } from "../../types/types";
+import { loremIpsum } from "lorem-ipsum";
+import React from "react";
+
+const ProductDetailsText = ({ product }: { product: Product }) => {
+  const [Text, setText] = React.useState("");
+
+  React.useEffect(() => {
+    const description = loremIpsum({
+      count: 3,
+      units: "sentence",
+    });
+
+    setText(description);
+  }, []);
+
+  return (
+    <Stack
+      direction="column"
+      spacing={3}
+      sx={{
+        mt: { xs: 3, md: 0 },
+      }}
+    >
+      <Typography component="h1" variant="h3">
+        {product.title}
+      </Typography>
+      <Typography>${product.price}</Typography>
+      <Divider />
+      <Typography>{Text}</Typography>
+    </Stack>
+  );
+};
+
+export default ProductDetailsText;
