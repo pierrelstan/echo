81aac1db2a6ce52f74381fb4b20de9638a85743e
Stanley Pierre Louis <stanleypl75@gmail.com>
2022-09-28 15:04:33 -0400

    fix: add shownavdrawer

--- components/common/Drawer/MenuDrawer.tsx
+++ components/common/Drawer/MenuDrawer.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
@@ -8,28 +8,10 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import MenuIcon from "@mui/icons-material/Menu";
 
-type Anchor = "left";
-
 export default function MenuDrawer() {
-  const [state, setState] = React.useState({
-    left: false,
-  });
+  const [showNavDrawer, setShowNavDrawer] = useState(false);
 
-  const toggleDrawer =
-    (anchor: Anchor, open: boolean) =>
-    (event: React.KeyboardEvent | React.MouseEvent) => {
-      if (
-        event.type === "keydown" &&
-        ((event as React.KeyboardEvent).key === "Tab" ||
-          (event as React.KeyboardEvent).key === "Shift")
-      ) {
-        return;
-      }
-
-      setState({ ...state, [anchor]: open });
-    };
-
-  const list = (anchor: Anchor) => (
+  const list = () => (
     <Box sx={{ maxWidth: "auto", width: "200px" }} role="presentation">
       <List>
         {["Home", "Pc", "Ps5", "Xbox"].map((text) => (
@@ -66,20 +48,16 @@ export default function MenuDrawer() {
 
   return (
     <div>
-      {(["left"] as const).map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>
-            <MenuIcon fontSize="medium" />
-          </Button>
-          <Drawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-          >
-            {list(anchor)}
-          </Drawer>
-        </React.Fragment>
-      ))}
+      <Button onClick={() => setShowNavDrawer(true)}>
+        <MenuIcon fontSize="medium" />
+      </Button>
+      <Drawer
+        anchor={"left"}
+        open={showNavDrawer}
+        onClose={() => setShowNavDrawer(false)}
+      >
+        {list()}
+      </Drawer>
     </div>
   );
 }
