47a9d60df4239e037b3c37f8e0c06b4cad876005
Stanley Pierre Louis <stanleypl75@gmail.com>
2022-10-03 18:35:53 -0400

    fix: update url / next-config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-  },
   eslint: {
     ignoreDuringBuilds: true
   },
--- utils/getProduct.util.ts
+++ utils/getProduct.util.ts
@@ -5,8 +5,10 @@ async function getProducts() {
   try {
     const {
       data: { products },
-    } = await axios.get(`${process.env.NEXT_PUBLIC_URL}` + routes.api.products);
-    console.log(process.env.NEXT_PUBLIC_URL);
+    } = await axios.get(
+      `${process.env.NEXT_PUBLIC_DATABASE_URL}` + routes.api.products
+    );
+    // console.log(process.env.NEXT_PUBLIC_URL);
     return products;
   } catch (error) {
     console.log(error);
