6ca0de10496a4b4075e63568f7e35748527e2e38
Stanley Pierre Louis <stanleypl75@gmail.com>
2022-10-06 19:51:40 -0400

    chore: remove data user

--- features/user/userSlice.spec.ts
+++ features/user/userSlice.spec.ts
@@ -1,20 +1,7 @@
+import { dummyDataUser } from "@/utils/test.utils";
 import userReducer, { login, logout } from "./userSlice";
 
 import { initialState } from "./userSlice";
-const dummyDataUser = {
-  user: {
-    isAuth: true,
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRlbW91c2VyQHRlc3QuY29tIiwiaWF0IjoxNjY0NDc3NTk2LCJleHAiOjE2NjQ1MTM1OTZ9.Dt5PbZlaQO-ogyK7wqiUV8fpxc_QItjZrSRLfyorskc",
-    user: {
-      _id: "631ca88585a9572ba6119c57",
-      email: "demouser@test.com",
-      createdAt: "2022-09-10T15:08:53.850Z",
-      password: "$2a$10$Cr7/9pAAmLyzRGJZsVYXJurL9H4/tZkdnJyMcjaDtgLvueBafFwGG",
-      updatedAt: "2022-09-10T15:08:53.850Z",
-    },
-  },
-};
 
 describe("user reducer", () => {
   it("should handle initial state", () => {
